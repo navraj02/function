f9ac817d407cce8b6fe75148e2c6bb4b98b24f29
navraj02 <baathnavraj1@gmail.com>
2025-07-07 11:43:31 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,133 @@
+#                       UPPER STRING
+
+a="navraj kaur"
+print(a.upper())
+
+
+#                      LOWER STRING
+
+a="navraj kaur"
+print(a.lower())
+
+#                     capitalize string
+
+a="navraj kaur"
+print(a.capitalize())
+
+#                     title string
+
+a="navraj kaur"
+print(a.title())
+
+#                   swapcase string
+
+a="navraj kaur"
+print(a.swapcase())
+
+#                  endswith string
+
+a="navraj kaur"
+print(a.islower())
+
+#                 isupper string    
+
+a="navraj kaur"
+print(a.isupper())
+
+#                  istitle string
+
+a="navraj kaur"
+print(a.istitle())
+
+#                  isalpha string
+
+a="navraj kaur"
+print(a.isalpha())
+
+#                   isalnum string
+
+a="navraj kaur"
+print(a.isalnum())
+
+#                   isdigit string
+
+a="navraj kaur"
+print(a.isdigit())
+
+#                    isspace
+
+a="navraj kaur"
+print(a.isspace())
+
+#                  endwith string    
+
+# a="navraj kaur"
+# print(a.endwith())
+
+#                  startwith string
+
+# a="navraj kaur"
+# print(a.startwith())
+
+#                 count string
+
+a="navraj kaur"
+print(a.count("n"))
+print(a.count("a"))
+print(a.count("v"))
+print(a.count("r"))
+print(a.count("j"))
+print(a.count("k"))
+print(a.count("u"))
+
+
+#                 find string
+
+a="navraj kaur"
+print(a.find("r"))
+print(a.find("g"))
+print(a.find("o"))
+#                 strip string 
+
+a="navraj kaur"
+print(a.strip())
+
+#                  istrip string
+
+# a="navraj kaur"
+# print(a.istrip())
+
+#                 rstrip string
+
+a="navraj kaur"
+print(a.rstrip())
+
+#                 split string
+
+a="navraj kaur"
+print(a.split())
+
+#                 ijust string
+
+# a="navraj kaur"
+# print(a.Ijust())
+
+#                 rjust string
+
+# a="navraj kaur"
+# print(a.rjust())
+
+#                zfill string
+
+# a="navraj kaur"
+# print(a.zfill())
+
+#               replace string
+
+# a="navraj kaur"
+# print(a.replace())
+
+#               joinstring
+
+# a="navraj kaur"
+# print(a.join())
