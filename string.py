73ee944d86f374055893fb76fecd9cca02d9faf7
navraj02 <baathnavraj1@gmail.com>
2025-07-09 11:25:35 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,89 @@
+#                       UPPER STRING
+
+def exupper(): 
+    word="navraj kaur"
+    return word.upper()
+print(exupper())
+
+#                      LOWER STRING
+def exlower():
+    word="navraj kaur"
+
+    return(word.lower())
+print(exlower()) 
+#                     capitalize string
+
+
+def excapitalize():
+    word="navraj kaur"    
+    return(word.capitalize())
+print(excapitalize())
+
+#                   swapcase string
+def exswapcase():
+    word="navraj kaur"
+    return(word.swapcase())
+print(exswapcase())
+#                  endswith string
+
+def exendswith():
+    word="navraj kaur"
+    return(word.islower())
+print(exendswith())
+#                 isupper string    
+def exisupper():
+    word="navraj kaur"
+    return(word.isupper())
+print(exisupper())
+#                  istitle string
+def existitle():
+    word="navraj kaur"
+    return(word.istitle())
+print(existitle())
+#                  isalpha string
+
+def exisalpha():
+    word="navraj kaur"
+    return(word.isalpha())
+print(exisalpha())
+#                   isalnum string
+def exisalnum():
+    word="navraj kaur"
+    return(word.isalnum())
+print(exisalnum())
+#                   isdigit string
+def exisdigit():
+    word="navraj kaur"
+    return(word.isdigit())
+print(exisdigit())
+#                    isspace
+
+def exisspace():
+    word="navraj kaur"
+    return(word.isspace())
+print(exisspace())
+
+#                 count string
+def excount():
+    word='red','blue''green','red','red'
+    return(word.count('red'))
+print(excount())
+
+#                 find string
+def exfind():
+    word="navraj kaur"
+    return(word.find("r"))
+print(exfind())   
+#                 strip string 
+
+def exstrip():
+    word="navraj kaur"
+    return(word.strip())
+print(exstrip())
+
+
+#                 rstrip string
+def exrstrip():
+    word="navraj kaur"
+    return(word.rstrip())
+print(exrstrip())
